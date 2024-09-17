5abbdec4b071c11cbc50f425266215f0d6455e6e
anil kumar <anilthirumala@live.com>
2024-09-17 14:24:01 +0530

    docker and docker-compose demo added

---
+++ DockerDemo/BuyerService/Controllers/BuyerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuyerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuyerController : ControllerBase
+    {
+        // GET: api/Buyer
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "John Doe", "Jane Smith", "Mike Johnson" };
+        }
+    }
+}
---
+++ DockerDemo/SellerService/Controllers/SellerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SellerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Seller Service";
+        }
+    }
+}
